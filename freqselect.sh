450b17d1446dcc005afafddba1d3b5d2db6f4703
Ricardo Valdez <rickondrius@gmail.com>
2021-07-30 18:28:45 -0400

    First commit

---
+++ freqselect.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Change cpu governor to performance
+# This must be run as root
+# Ricardo Valdez @driconrikus
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+else
+
+# Query current CPU governor
+cpufreq=$(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | head -1)
+echo "Your current CPU governor is: $cpufreq"
+
+# CPU Governor interactive selector
+echo "Please select cpu governor:
+	1. Performance (full CPU frequency, kills battery)
+	2. Ondemand    (Balanced CPU frequency, saves battery)
+	3. Powersave   (Low CPU frequency, saves even more battery)
+	"
+
+read option
+	case $option in
+		1)
+		  echo "Setting performance governor..."
+		  cpupower frequency-set --governor performance
+		  echo "Done!"
+		 ;;
+		2)
+		  echo "Setting performance to Ondemand..."
+		  cpupower frequency-set --governor ondemand
+		  echo "Done!"
+		 ;;
+		3)
+		  echo "Setting performance to Powersave..."
+		  cpupower frequency-set --governor powersave
+		  echo "Done!"
+		 ;;
+		 *)
+			echo "Invalid option. Please choose 1, 2 or 3"
+	 	 ;;
+	esac
+			
+fi
