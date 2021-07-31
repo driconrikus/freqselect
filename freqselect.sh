d2b2951364e04dac7d3f42d9d7c93d2003683445
Ricardo Valdez <rickondrius@gmail.com>
2021-07-30 21:18:46 -0400

    Added Enable/Disable placeholder, also added a while loop to the selector with an exit option

--- freqselect.sh
+++ freqselect.sh
@@ -16,17 +16,20 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as ${RED}root!${NC}"
    exit 1
 else
-
+while true; do
 # Query current CPU governor
 cpufreq=$(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | head -1)
 echo -e "Your current CPU governor is: ${GREEN}${BOLD} ${cpufreq} ${NC}"
-
+#Persistance status placeholder
+echo -e "Persistance status: ${RED}${BOLD}Inactive${NC}" 
 # CPU Governor interactive selector
 echo -e "Please select a CPU governor:
-	1. performance ${DIM}(Best performance. Full CPU frequency at all times, increases CPU temperatures, ${BOLD}reduces battery life significantly.)${NC}
+	1. performance ${DIM}(Best performance. Full CPU frequency at all times, ${BOLD}reduces battery life significantly.)${NC}
 	2. ondemand    ${DIM}(Balanced performarmance. CPU frequency scales depending on current system load, ${BOLD}average battery life.)${NC}
 	3. powersave   ${DIM}(Slow performance. Low CPU frequency at all times, ${BOLD}increases battery life significantly.)${NC}
 	4. schedutil   ${DIM}(Defaut setting. Similar to ondemand. ${BOLD}Average battery life.)${NC}
+	5. Enable/Disable persistance.
+	6. Exit
 	"
 # Grab user input
 read option
@@ -35,25 +38,38 @@ read option
 		  echo -e "Setting governor to ${BLUE}${BOLD}performance${NC}..."
 		  cpupower frequency-set --governor performance
 		  echo "Done!"
+		  exit 0
 		 ;;
 		2)
 		  echo -e "Setting governor to ${BLUE}${BOLD}ondemand${NC}..."
 		  cpupower frequency-set --governor ondemand
 		  echo "Done!"
+		  exit 0
 		 ;;
 		3)
 		  echo -e "Setting governor to ${BLUE}${BOLD}powersave${NC}..."
 		  cpupower frequency-set --governor powersave
 		  echo "Done!"
+		  exit 0
 		 ;;
 		4)
 		  echo -e "Setting governor to ${BLUE}${BOLD}schedutil${NC}..."
 		  cpupower frequency-set --governor schedutil
 		  echo "Done!"
+		  exit 0
 		 ;;
-		 *)
-			echo "Invalid option. Please choose a number from above!"
-			exit 1
+		5)
+		  echo -e "${GREEN}Persistance feature not available yet!${NC} \n"
+		  sleep 3
+		 ;;
+		6)
+		  echo "Bye!"
+		  exit 0
+		  ;;
+		*)
+			echo -e "${RED}Invalid option${NC}. Please choose a number from above! \n"
+			sleep 3
 	 	 ;;
-	esac		
+	esac
+	done		
 fi
