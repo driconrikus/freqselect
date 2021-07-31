982c6f8d6c9a7079d830805939bb3098442907e8
Ricardo Valdez <rickondrius@gmail.com>
2021-07-30 22:07:47 -0400

    Initial support for persistance.

--- freqselect.sh
+++ freqselect.sh
@@ -13,7 +13,7 @@ BOLD='\e[1m'
 
 # Check if user is root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as ${RED}root!${NC}"
+   echo -e "This script must be run as ${RED}root!${NC}"
    exit 1
 else
 while true; do
@@ -21,15 +21,21 @@ while true; do
 cpufreq=$(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | head -1)
 echo -e "Your current CPU governor is: ${GREEN}${BOLD} ${cpufreq} ${NC}"
 #Persistance status placeholder
-echo -e "Persistance status: ${RED}${BOLD}Inactive${NC}" 
+persistance=$(systemctl status cpupower.service | grep "Succeeded" | awk '{print $7}')
+if [[ $persistance == "Succeeded." ]]; then
+echo -e "Persistance status: ${GREEN}${BOLD}Active${NC}" 
+else
+echo -e "Persistance status: ${RED}${BOLD}Inactive${NC}"
+fi
 # CPU Governor interactive selector
 echo -e "Please select a CPU governor:
 	1. performance ${DIM}(Best performance. Full CPU frequency at all times, ${BOLD}reduces battery life significantly.)${NC}
 	2. ondemand    ${DIM}(Balanced performarmance. CPU frequency scales depending on current system load, ${BOLD}average battery life.)${NC}
 	3. powersave   ${DIM}(Slow performance. Low CPU frequency at all times, ${BOLD}increases battery life significantly.)${NC}
 	4. schedutil   ${DIM}(Defaut setting. Similar to ondemand. ${BOLD}Average battery life.)${NC}
-	5. Enable/Disable persistance.
-	6. Exit
+	5. Enable persistance. ${DIM}(Keeps settings across reboots.)${NC}
+	6. Disable persistance.
+	7. Exit
 	"
 # Grab user input
 read option
@@ -59,10 +65,35 @@ read option
 		  exit 0
 		 ;;
 		5)
-		  echo -e "${GREEN}Persistance feature not available yet!${NC} \n"
+		  pers_file=/etc/systemd/system/cpupower.service
+		  if [ -f "$pers_file" ]; then
+    			echo -e "${GREEN}Service is already enabled!${NC} \n"
+				sleep 3
+		  else 
+		  echo -e "Applying persistance... \n"
+		  echo "[Unit]
+Description=cpufreq governor persistance
+[Service]
+Type=simple
+ExecStart=/usr/bin/cpupower -c all frequency-set --governor ${cpufreq}
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/cpupower.service
+		  echo "Enabling persistance..."
+		  systemctl daemon-reload
+		  systemctl enable cpupower.service --now
+		  echo -e "Done! \n"
 		  sleep 3
+		  fi
 		 ;;
 		6)
+			echo -e "Disabling persistance..."
+			systemctl disable cpupower.service --now
+			rm /etc/systemd/system/cpupower.service
+			systemctl daemon-reload
+			echo -e "Done! \n"
+			sleep 3
+		 ;;
+		7)
 		  echo "Bye!"
 		  exit 0
 		  ;;
